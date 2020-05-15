d3eeaead8a5df44f495b0d9ebcf456a24719333e
rhmeyer314 <rhmeyer314@gmail.com>
2020-05-15 14:39:51 -0400

    added CORS fix

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.post("/email", (req, res) => {
-    res.json({Message: "Email Recieved"})
+    res.json({Message: "Email Recieved"});
+    console.log(req.body);
 })
 
 app.listen(8080, () => {
