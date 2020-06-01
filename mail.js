afb76366034a6299f1734088c09cbd7bc03eaee7
Robert Meyer <45831888+rhmeyer314@users.noreply.github.com>
2020-05-31 21:53:26 -0400

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,8 +3,8 @@ const mailgun = require('nodemailer-mailgun-transport');
 
 const auth = {
     auth: {
-        api_key: '6e31b6589e1725c9b856181ef82e64bc-3e51f8d2-e1c7fcd8',
-        domain: 'sandbox96299ebe71424b2f966485a26fa1278c.mailgun.org'
+        api_key: 'something secret',
+        domain: 'something secret'
     }
 }
 
